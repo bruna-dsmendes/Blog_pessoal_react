c9e0d88a9947a786d00a5511588baf89719c3527
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-06-26 22:02:09 -0300

    Atualizando  o Componente Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('O usuário foi desconectado com sucesso!')
+    navigate('/')
+  }
   return (
     <>
       <div className="w-full flex justify-center py-4
@@ -14,7 +25,7 @@ function Navbar() {
             Temas
             Cadastrar Tema
             Perfil
-            Sair
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
           </div>
         </div>
       </div>
