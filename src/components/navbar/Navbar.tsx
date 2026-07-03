b48d395abf2eafa7e91d67d72eaa096496fb6e2f
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-07-03 09:14:22 -0300

    Alteração da lógica de exibição do navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,23 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, type ReactNode } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 
   const navigate = useNavigate();
-  const { handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
     handleLogout()
     alert('O usuário foi desconectado com sucesso!')
     navigate('/')
   }
-  return (
-    <>
+
+  let component: ReactNode
+
+  if (usuario.token !== "") {
+    component = (
+
       <div className="w-full flex justify-center py-4
   bg-sky-100 text-sky-900">
 
@@ -29,6 +33,11 @@ function Navbar() {
           </div>
         </div>
       </div>
+    )
+  }
+  return (
+    <>
+      {component}
     </>
   )
 }
